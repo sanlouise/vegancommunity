9d9a4fef3b7b6c3e0cb148f5652ef0c4fa66104e
sandralouise <sandra.adamshallie@gmail.com>
2016-05-06 18:58:41 +0000

    Update bashrc variables for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
+  
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
@@ -12,4 +13,5 @@ ActionMailer::Base.smtp_settings = {
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
+  
 }
