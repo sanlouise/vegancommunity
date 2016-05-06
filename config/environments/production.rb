1b952abd3b7e6da4e7996e12ea74522b00c15d51
sandralouise <sandra.adamshallie@gmail.com>
2016-05-06 18:52:46 +0000

    Update production variables for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.serve_static_assets = true
-  config.assets.compile = true
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -23,9 +20,16 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.serve_static_files = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
