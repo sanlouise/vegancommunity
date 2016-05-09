58806481970266d3038006a3c5467e63801556c3
sandralouise <sandra.adamshallie@gmail.com>
2016-05-09 15:57:49 +0000

    Add ingredient search interface

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-
-});
-
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+  
 (function($){
   $(function(){
     $('.button-collapse').sideNav();
