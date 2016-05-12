7b922450e22d22e68d422fb6eec5df9850855a00
sandralouise <sandra.adamshallie@gmail.com>
2016-05-12 05:16:18 +0000

    Add validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,5 @@ class Meal < ActiveRecord::Base
   has_many :recipes, through: :recipe_meals
   has_many :recipe_meals
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 25 }, uniqueness: {case_sensitive: false}
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,10 +7,16 @@ class Recipe < ActiveRecord::Base
   # Join Table for Ingredients and Recipes
   has_many :ingredients, through: :recipe_ingredients
   has_many :recipe_ingredients
-  
   has_many :comments
   mount_uploader :image, ImageUploader
+  
   validate :image_size
+  validates :image, presence: true
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {minimum: 4, maximum: 60}
+  validates :description, presence: true, length: {minimum: 50, maximum: 1000}
+  validates :directions, presence: true, length: {minimum: 20, maximum: 5000}
+  validates :ingredients, presence: true
   
   private
   def image_size
